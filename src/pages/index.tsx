80f9716e72cbadae3c5358e9d9653f4135cc1b15
Wagner Andrade Linhares Da Silva <wagner.a.silva@kroton.com.br>
2022-10-11 00:21:42 -0300

    Challenge finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,13 +24,89 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+  async function handleLoadDataFromNextPagePrismic() {
+    if (!postsPagination.next_page) return;
 
-//   // TODO
-// };
+    const response = await fetch(postsPagination.next_page);
+
+    response.json().then(result => {
+      const newPosts = getPostsFormatted(result.results);
+
+      setPosts([...posts, ...newPosts]);
+      setNextPage(result.next_page);
+    });
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Home | Blog desafio</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <Header />
+        <div className={styles.posts}>
+          {posts.map(result => (
+            <Link key={result.uid} href={`/post/${result.uid}`}>
+              <a>
+                <section className={styles.contentPost}>
+                  <h1>{result.data.title}</h1>
+                  <h2>{result.data.subtitle}</h2>
+                  <div className={styles.dayAuthorInfo}>
+                    <div>
+                      <FiCalendar />
+                      <span>
+                        {format(
+                          new Date(result.first_publication_date),
+                          'dd MMM yyyy',
+                          { locale: ptBR }
+                        )}
+                      </span>
+                    </div>
+                    <div>
+                      <FiUser />
+                      <span>{result.data.author}</span>
+                    </div>
+                  </div>
+                </section>
+              </a>
+            </Link>
+          ))}
+        </div>
+        {nextPage && (
+          <button
+            className={styles.highlightButton}
+            type="button"
+            onClick={handleLoadDataFromNextPagePrismic}
+          >
+            Carregar mais posts
+          </button>
+        )}
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.subtitle', 'post.author'],
+    }
+  );
+
+  const results = getPostsFormatted(postsResponse.results);
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        results,
+      },
+    },
+  };
+};
